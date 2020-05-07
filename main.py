1f1b5e19c55561a088296d632f55c5098d45e014
Szymek <sprzepiora@hotmail.co.uk>
2020-05-07 10:39:05 +0200

    Testowanie czasu i czatu głowosowego

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def price(cena, index: int = 0):
     for pizza in table:
         pizza_name = pizza.find_all('strong')
         pizza_price = pizza.find_all('td', class_='t3b')
-
+    
     await cena.send(f'Cena {pizza_name[index].text} to {pizza_price[index].text}')
 
 
@@ -36,5 +36,21 @@ async def time(czas):
 
     await czas.send(f'Od ostatniej zmiany ceny upłyneło {days} dni')
 
+
+@bot.command(name='test')
+async def test():
+    guild = bot.get_guild(706851463816806453)
+    channel_list = [len(channel.members) for channel in guild.voice_channels]
+    await guild.voice_channels[channel_list.index(max(channel_list))].connect()
+
+
+@bot.command(name='papal')
+async def papal(papal):
+    while True:
+        print(datetime.datetime.now().time())
+        if datetime.datetime.now().hour == 21 and datetime.datetime.now().minute == 37:
+            print('test')
+            await papal.send('Papal')
+            break
 
 bot.run(TOKEN)

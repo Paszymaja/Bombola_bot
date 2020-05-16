e118887f583507783ece64caf68da158db22a41e
Szymek <sprzepiora@hotmail.co.uk>
2020-05-16 16:07:19 +0200

    Cleanup and requirements

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import datetime
 
 from bs4 import BeautifulSoup
 from discord.ext import commands
-from dotenv import load_dotenv
 
-load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')

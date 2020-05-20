f79eae4be236504db3001339659a00beaed964c2
Szymek <sprzepiora@hotmail.co.uk>
2020-05-20 17:45:36 +0200

    Made function from method

--- Bombola.py
+++ Bombola.py
@@ -7,9 +7,23 @@ from bs4 import BeautifulSoup
 from discord.ext import commands, tasks
 
 
+def price_check(index):
+    pizza_name, pizza_price = [], []
+    url = 'https://www.pizzeriabombola.pl/menu.php'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    table = soup.find_all('div', attrs={'style': 'float: right; width: 450px; margin-top:5px;'})
+
+    for pizza in table:
+        pizza_name = pizza.find_all('strong')
+        pizza_price = pizza.find_all('td', class_='t3b')
+
+    return pizza_name[index].text, pizza_price[index].text
+
+
 class Bombola(commands.Cog):
     def __init__(self, bot):
-        self.last_price = self.price_check(index=0)[1][:-3]
+        self.last_price = price_check(index=0)[1][:-3]
         self.bot = bot
         self.timer.add_exception_type(asyncpg.PostgresConnectionError)
         self.timer.start()
@@ -17,22 +31,9 @@ class Bombola(commands.Cog):
     def cog_unload(self):
         self.timer.cancel()
 
-    def price_check(self, index):
-        pizza_name, pizza_price = [], []
-        url = 'https://www.pizzeriabombola.pl/menu.php'
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        table = soup.find_all('div', attrs={'style': 'float: right; width: 450px; margin-top:5px;'})
-
-        for pizza in table:
-            pizza_name = pizza.find_all('strong')
-            pizza_price = pizza.find_all('td', class_='t3b')
-
-        return pizza_name[index].text, pizza_price[index].text
-
     @commands.command(name='cena', help='Aktualna cena pizzy w bomboli')
     async def price(self, ctx, index: int = 0):
-        pizza_name, pizza_price = self.price_check(index)
+        pizza_name, pizza_price = price_check(index)
 
         ctx_message = f'Cena {pizza_name} to {pizza_price}'
 
@@ -53,7 +54,7 @@ class Bombola(commands.Cog):
 
     @tasks.loop(minutes=10)
     async def timer(self):
-        current_price = self.price_check(index=0)[1][:-3]
+        current_price = price_check(index=0)[1][:-3]
         if current_price != self.last_price:
             print('Cena się zmieniła')
             self.timer.stop()

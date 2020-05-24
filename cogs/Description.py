d6999827926e188745d251d53061ccedb333bee2
Szymek <sprzepiora@hotmail.co.uk>
2020-05-24 11:25:57 +0200

    `cogs package`

--- cogs/Description.py
+++ cogs/Description.py
@@ -3,18 +3,21 @@ from itertools import cycle
 import discord
 from discord.ext import commands, tasks
 
+from cogs import Bombola
+
 
 class Description(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.status = ['Pizza', 'Bombola', 'Sycylijska', '🍕']
+        self.status = [f' 🍕 Bombola : {Bombola.price_check(index=0)[1]}',
+                       f' 🍕 Sycylijska : {Bombola.price_check(index=3)[1]}']
         self.change_status.start()
         self.msgs = cycle(self.status)
 
     def cog_unload(self):
         self.change_status.cancel()
 
-    @tasks.loop(minutes=10)
+    @tasks.loop(minutes=1)
     async def change_status(self):
         current_status = next(self.msgs)
         await self.bot.change_presence(activity=discord.Game(name=current_status))

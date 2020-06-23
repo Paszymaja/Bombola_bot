99f2ae506cee24c77a6c29c9d3bdb07dd3523afb
paszymaja <sprzepiora@hotmail.co.uk>
2020-06-23 15:29:01 +0200

    markov chain

--- cogs/ServerUtils.py
+++ cogs/ServerUtils.py
@@ -1,6 +1,9 @@
 import datetime
 import json
 import random
+import markovify
+import os
+import pyAesCrypt
 
 from discord.ext import commands, tasks
 
@@ -13,6 +16,11 @@ class ServerUtils(commands.Cog):
         self.main_channel_id = self.config_data['main_channel_id']
         self.daily_thoughts.start()
         self.messages = []
+        # Markov chain variables
+        self.enc_key = os.getenv('ENC_KEY')
+        pyAesCrypt.decryptFile('data/messages/messages.txt', 'temp/data_out.txt', self.enc_key, bufferSize=10 * 1024)
+        self.chain_data = json.load(open('temp/data_out.txt', 'r', encoding='utf-8'))
+        self.text_model = markovify.Text(' '.join(self.chain_data))
 
     def cog_unload(self):
         self.daily_thoughts.cancel()
@@ -33,3 +41,12 @@ class ServerUtils(commands.Cog):
         channel = self.bot.get_channel(self.thoughts_id)
         messages = await channel.history(limit=200).flatten()
         self.messages = [message.content for message in messages]
+
+    @commands.command(name='tekst', help='Markov Chain do rozmowy.')
+    async def chain(self, ctx):
+        if (chain := self.text_model.make_sentence()) is None:
+            ctx_message = 'Coś poszło nie tak. Spróbuj jeszcze raz.'
+        else:
+            ctx_message = chain
+        print(ctx_message)
+        await ctx.channel.send(ctx_message)
